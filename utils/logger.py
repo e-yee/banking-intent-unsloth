5405b743112565822f4b0d05338e9c34455f9741
e-yee <viduydmm@gmail.com>
2026-04-24 17:25:44 +0700

    feat(utils): add utils

---
+++ utils/logger.py
@@ -0,0 +1,38 @@
+import logging
+import colorlog
+
+logging.addLevelName(logging.INFO, "Info")
+logging.addLevelName(logging.WARNING, "Warning")
+
+SUCCESS_LEVEL = 25
+logging.addLevelName(SUCCESS_LEVEL, "Success")
+
+def success(self, message, *args, **kwargs):
+    """Success logging function for logger."""
+    if self.isEnabledFor(SUCCESS_LEVEL):
+        self._log(SUCCESS_LEVEL, message, args, **kwargs)
+        
+logging.Logger.success = success
+
+
+def get_logger(name: str = __name__) -> logging.Logger:
+    """Get logger."""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        
+        formatter = colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s:%(reset)s %(message)s",
+            log_colors={
+                "Info": "blue",
+                "Success": "green",
+                "Warning": "yellow"
+            }
+        )
+        
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
+    return logger
---
+++ utils/paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+DATA_DIR = BASE_DIR / "sample_data"
+CONFIG_DIR = BASE_DIR / "configs"
