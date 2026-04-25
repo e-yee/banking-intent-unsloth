40573621e5676d4c15c952225c2f0614d6f035cc
e-yee <viduydmm@gmail.com>
2026-04-25 12:34:41 +0700

    refactor(scripts): refactor label format

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -25,11 +25,11 @@ def download_dataset(
     output_path.mkdir(parents=True, exist_ok=True)
     
     # Converts to Polars
-    train_pl = pl.from_arrow(dataset["train"].data.table)
-    logger.info(f"Loaded {train_pl.shape[0]:,} rows (train data).")
+    train_df = pl.from_arrow(dataset["train"].data.table)
+    logger.info(f"Loaded {train_df.shape[0]:,} rows (train data).")
     
-    test_pl = pl.from_arrow(dataset["test"].data.table)
-    logger.info(f"Loaded {test_pl.shape[0]:,} rows (test data).")
+    test_df = pl.from_arrow(dataset["test"].data.table)
+    logger.info(f"Loaded {test_df.shape[0]:,} rows (test data).")
     
     # Labels
     label_feature = dataset["train"].features.get("label")
@@ -53,26 +53,26 @@ def download_dataset(
     train_path = output_path / "train.csv"
     test_path = output_path / "test.csv"
     
-    train_pl.write_csv(train_path)
-    test_pl.write_csv(test_path)
+    train_df.write_csv(train_path)
+    test_df.write_csv(test_path)
     
     logger.success("Dataset saved successfully.")
     
-    return train_pl, test_pl, labels
+    return train_df, test_df, labels
 
 def clean_text(text: str) -> str:
     """Lower case and strip extra spaces of the given text."""
     return " ".join(text.lower().split())
     
 def preprocess_data(
-    train_data: pl.DataFrame,
-    test_data: pl.DataFrame,
+    train_df: pl.DataFrame,
+    test_df: pl.DataFrame,
     labels: list
 ) -> tuple:
     """Preprocess raw data."""
     
-    label_map = {f"L<{i}>":label for i, label in enumerate(labels)}
-    mapping = {i:f"L<{i}>" for i in range(len(labels))}
+    label_map = {f"{i+1}":label for i, label in enumerate(labels)}
+    mapping = {i:i+1 for i in range(len(labels))}
     
     def preprocess(data: pl.DataFrame) -> pl.DataFrame:
         return (
@@ -83,7 +83,7 @@ def preprocess_data(
                 .alias("preprocessed_text"),
                 
                 pl.col("label")
-                .replace_strict(mapping, return_dtype=pl.String)
+                .replace_strict(mapping)
                 .alias("label_encoded")
             )
             .select(["preprocessed_text", "label_encoded"])
@@ -92,8 +92,8 @@ def preprocess_data(
     # Preprocess data
     logger.info("Preprocessing dataset...")
     
-    preprocessed_train_data = preprocess(train_data)
-    preprocessed_test_data = preprocess(test_data)
+    preprocessed_train_df = preprocess(train_df)
+    preprocessed_test_df = preprocess(test_df)
     
     # Save data
     output_path = DATA_DIR / "preprocessed"
@@ -104,15 +104,15 @@ def preprocess_data(
     train_path = output_path / "train.csv"
     test_path = output_path / "test.csv"
     
-    preprocessed_train_data.write_csv(train_path)
-    preprocessed_test_data.write_csv(test_path)
+    preprocessed_train_df.write_csv(train_path)
+    preprocessed_test_df.write_csv(test_path)
     
     with open(output_path / "label_map.json", "w", encoding="utf-8") as f:
         json.dump(label_map, f, indent=2, ensure_ascii=False)
     
     logger.success("Preprocessed dataset saved successfully.")
     
-    return preprocessed_train_data, preprocessed_test_data, label_map
+    return preprocessed_train_df, preprocessed_test_df, label_map
 
 def main():
     try:
